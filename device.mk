a9351ede24bb54c13df07c937e18dde7836143f0
Santosh Mardi <gsantosh@codeaurora.org>
2020-05-12 14:08:02 +0530

    beryllium: rootdir: move memlat settings to separate script
    Kernel security hooks were added in kernel to restrict any
    userspace process or thread to open perf events, this change
    needs elevated selinux permission for perf activity.
    
    Move the settings to a new script and give selinux permission
    for perf activity for this new script.
    
    Change-Id: Ia9f603d8fce101b71bad89bf00a445f31bfbf5ea
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- device.mk
+++ device.mk
@@ -167,7 +167,9 @@ PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sensors.sh \
-    init.qcom.sh
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.dcvs.sh
 
 # Context Hub
 PRODUCT_PACKAGES += \
