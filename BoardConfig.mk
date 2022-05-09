5a7aa59b808192907172458ff8f19ee76bd73931
BladeRunner-A2C <prisoner56898@protonmail.com>
2022-05-10 00:57:32 +0600

    beryllium: Set up HIDL matrix from QCOM common
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>
    Change-Id: Idae7e57866599697a8a32c8b7ea1bea8d11c506c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,8 +57,10 @@ MAX_EGL_CACHE_SIZE := 2048*1024
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
      vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE := \
+     $(DEVICE_PATH)/configs/hidl/manifest.xml
+
+DEVICE_MATRIX_FILE += device/qcom/common/compatibility_matrix.xml
 
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 1
