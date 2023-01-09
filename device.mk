b64aff90a93a075e80fc9b1e93e03ee851670cd2
PainKiller3 <ninadpatil100@gmail.com>
2023-01-09 19:21:40 +0530

    beryllium: Import CarrierConfig Overlay
    * From beryllium-user-10-QKQ1.190828.002-V12.0.3.0.QEJMIXM-release-keys
    
    Change-Id: I5eb27fdd78633d049832008789ffa9f6208b218d

--- device.mk
+++ device.mk
@@ -376,6 +376,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 PRODUCT_PACKAGES += \
+    BerylliumCarrierConfigOverlay \
     BerylliumFaceUnlockServiceOverlay \
     BerylliumFrameworksOverlay \
     BerylliumNoCutoutOverlay \
